a2cddf6478a23f13a6d041808cb64d111ab8cc6e
rdanieldesign <rdanieldesign@gmail.com>
2014-10-05 08:22:35 -0400

    Initial Commit

---
+++ dist/scripts/main.js
@@ -0,0 +1,15 @@
+$('.toggle').click(function(){
+  $(this).toggleClass('hidden');
+});
+
+// $('.guess').click(function(){
+//   $(this).css('display', 'none');
+//   $('h2').css('display', 'block');
+//   $('.toggle').toggleClass('hidden');
+// });
+//
+// $('h2').click(function(){
+//   $(this).css('display', 'none');
+//   $('.guess').css('display', 'block');
+//   $('.toggle').toggleClass('hidden');
+// });
